e6e3c6c95587efdcae2a1e6d981d3480c040104d
21-manic-020g <21-manic-020g@21-manic-020>
2024-03-21 12:58:16 +0900

    정올 문제 506번 풀이 완료

---
+++ Jungol/src/q506/Main.java
@@ -0,0 +1,17 @@
+package q506;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var1 = 170;
+		double var2 = 68.6;
+
+		System.out.println("My height");
+		System.out.println(var1);
+		System.out.println("My weight");
+		System.out.printf("%f", var2);
+		
+	}
+
+}
